ec6e9d79e26ab7737fc486cc2b8b1373e03237d3
Savitha2000 <52274179+Savitha2000@users.noreply.github.com>
2020-02-06 22:30:00 +0530

    Update strong number.py

--- strong number.py
+++ strong number.py
@@ -1,17 +1,23 @@
-sum1=0
-num=int(input())
-temp=num
-while(num):
+sum=0
+a=int(input())
+temp=a
+while(a):
     i=1
     f=1
-    r=num%10
+    r=a%10
     while(i<=r):
         f=f*i
         i=i+1
-    sum1=sum1+f
-    num=num//10
-if(sum1==temp):
+        sum=sum+f
+        a=a//10
+if (sum==temp):
     print("The number is a strong number")
+    
 else:
     print("The number is not a strong number")
     
+    
+            
+        
+        
+            
