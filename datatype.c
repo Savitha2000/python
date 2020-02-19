3e1717f4ac38b5d0e3bc6775eff44fbd00db1f60
Savitha2000 <52274179+Savitha2000@users.noreply.github.com>
2020-02-19 14:30:44 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    long int aadhar=653212347836;
+    long int mobile=9842098420;
+    long int pincode=600028;
+    printf("%ld\n",aadhar);
+    printf("%ld\n",mobile);
+    printf("%ld\n",pincode);
+    
+}
