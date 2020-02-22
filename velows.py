275e883efb23ee60593c0e597342f8b192419f8b
Savitha2000 <52274179+Savitha2000@users.noreply.github.com>
2020-02-22 10:26:56 +0530

    Create velows.py

---
+++ velows.py
@@ -0,0 +1,16 @@
+a=input()
+b=['a','e','i','o','u','A','E','I','O','U']
+l=[]
+l1=[]
+for i in a :
+    if  i in b:
+        l.append(i)
+    if i not in b:
+        l1.append(i)
+for j in l:
+    print(j,end=" ")
+print()
+for k in l1:
+    print(k,end="")
+    
+        
