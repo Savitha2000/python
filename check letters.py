e2445c532453acbeffe93082372460d5e3b11b91
Savitha2000 <52274179+Savitha2000@users.noreply.github.com>
2020-02-24 12:06:37 +0530

    Create check letters.py

---
+++ check letters.py
@@ -0,0 +1,13 @@
+d=input()
+l1=['c','e','r','s','u']
+l=len(d)
+count=0
+for i in d:
+    if i in l1:
+        count+=1
+    else:
+        print("NO")
+if count==l:
+    print("YES")
+///ip cruse ,rucuse
+op yes
