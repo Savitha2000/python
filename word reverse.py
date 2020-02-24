77f73a24f5b275bf7556cb81aef199c649bed674
Savitha2000 <52274179+Savitha2000@users.noreply.github.com>
2020-02-24 13:32:47 +0530

    Create word reverse.py

---
+++ word reverse.py
@@ -0,0 +1,14 @@
+a=list(map(str,input().split(" ")))
+l=[]
+for i in a:
+   d=i[0]
+   f=i[-1]
+   g=i[1:-1][::-1]
+   p=d+g+f
+   l.append(p)
+for j in l:
+   print(j,end=" ")
+   
+   
+   //ip python online compiler
+   //op  
