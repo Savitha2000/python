da221769b3bf25469082ec282b9c645c97f4c3b8
Savitha2000 <52274179+Savitha2000@users.noreply.github.com>
2020-02-27 10:55:29 +0530

    Update swapcase withot string .py

--- swapcase withot string .py
+++ swapcase withot string .py
@@ -8,3 +8,20 @@ a,b=32,56
 p=cse(a,b)
 cse.swap(p)
 print(p.a,p.b)
+
+--------------------------------------------------
+class cse: 
+       def swap(self,a,b):
+        self.a=a
+        self.b=b
+        self.a,self.b=self.b,self.a
+a,b=32,56
+p=cse()
+cse.swap(p,a,b)
+print(p.a,p.b)
+
+
+
+
+
+
